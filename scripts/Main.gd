ab00b188b6f0597fe8ea3d90f4a1eae9a02dabad
Antonio <antonio.alvarez.chavarria@gmail.com>
2021-12-31 13:31:02 -0600

    the meteor spawning was fixed and now the meteor sprite always aims to the front

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -39,8 +39,8 @@ func _on_MobTimer_timeout():
 	# Set the mob's position to a random location.
 	mob.position = $MobPath/MobSpawnLocation.position
 	# Add some randomness to the direction.
-	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+#	direction += rand_range(-PI / 4, PI / 4)
+#	mob.rotation = direction
 	# Set the velocity (speed & direction).
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
