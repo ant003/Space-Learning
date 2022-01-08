6576e988c43da5530747901f359c60b00d7d47d2
Antonio <antonio.alvarez.chavarria@gmail.com>
2022-01-08 11:56:44 -0600

    a new coin is added to the screen after the last one is catched

--- scripts/Coin.gd
+++ scripts/Coin.gd
@@ -3,13 +3,17 @@ signal hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hide()
+	#hide()
+	pass
 
 func _on_Coin_area_entered(area):
 	emit_signal("hit")
-	queue_free()
+#	$CollisionShape2D.set_deferred("disabled", true)
+	call_deferred("queue_free") # 
+	#queue_free()
 	
 func start(pos):
 	position = pos
 	show()
+#	$CollisionShape2D.disabled = false
 
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,31 +1,50 @@
 extends Node
 
 export (PackedScene) var Mob
+export (PackedScene) var Coin
 var score
+var screen_size  # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+#	screen_size = get_viewport().get_rect().size
 
 func update_score():
 	score += 100
 	$HUD.update_score(score)
 
+func new_coin():
+	var coin = Coin.instance()
+#	Deferred ensures
+	call_deferred("add_child", coin)
+	var x_range = Vector2(50, 974)
+	var y_range = Vector2(50, 550)
+	var random_x = randi() % int(x_range[1]- x_range[0]) + 1 + x_range[0] 
+	var random_y =  randi() % int(y_range[1]-y_range[0]) + 1 + y_range[0]
+	var random_pos = Vector2(random_x, random_y)
+	coin.position = random_pos
+	coin.connect("hit", self, "new_coin")
+	coin.connect("hit", self, "update_score")
+	
+
 func gamer_over():
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
+	get_tree().call_group("coins", "queue_free")
 	$Music.stop()
 	$DeathSound.play() 
 	
 func new_game():
 	score = 0
-	$Coin.start($CoinPosition.position)
+	new_coin()
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Prepárate")
 	$Music.play()
+	
 
 
 func _on_MobTimer_timeout():
@@ -33,7 +52,8 @@ func _on_MobTimer_timeout():
 	$MobPath/MobSpawnLocation.offset = randi()
 	# Create a Mob instance and add it to the scene.
 	var mob = Mob.instance()
-	add_child(mob)
+#	add_child(mob)
+	call_deferred("add_child", mob)
 	# Set the mob's direction perpendicular to the path direction.
 	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
 	# Set the mob's position to a random location.
