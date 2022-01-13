844cad41fdf0ce5c3194d35e7bbbe4f46b09471c
Antonio <antonio.alvarez.chavarria@gmail.com>
2022-01-13 17:10:11 -0600

    Emeralds now add 500 points to the score and are cleared after the game finish

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -9,24 +9,26 @@ var screen_size  # Size of the game window.
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-#	screen_size = get_viewport().get_rect().size
 
-func update_score():
-	score += 100
+func update_score(points):
+	score += points
 	$HUD.update_score(score)
-
-func new_coin():
-	var coin = Coin.instance()
-#	Deferred ensures the function is thread safe
-	call_deferred("add_child", coin)
+	
+func gen_rand_pos():
 	var x_range = Vector2(50, 974)
 	var y_range = Vector2(50, 550)
 	var random_x = randi() % int(x_range[1]- x_range[0]) + 1 + x_range[0] 
 	var random_y =  randi() % int(y_range[1]-y_range[0]) + 1 + y_range[0]
 	var random_pos = Vector2(random_x, random_y)
-	coin.position = random_pos
+	return random_pos
+
+func new_coin():
+	var coin = Coin.instance()
+#	Deferred ensures the function is thread safe
+	call_deferred("add_child", coin)
+	coin.position = gen_rand_pos()
 	coin.connect("hit", self, "new_coin")
-	coin.connect("hit", self, "update_score")
+	coin.connect("hit", self, "update_score", [100])
 	
 func new_emerald():
 	$HUD.update_tip()
@@ -34,14 +36,9 @@ func new_emerald():
 	yield(get_tree().create_timer(10), "timeout")
 	var emerald = Emerald.instance()
 	call_deferred("add_child", emerald)
-	var x_range = Vector2(50, 974)
-	var y_range = Vector2(50, 550)
-	var random_x = randi() % int(x_range[1]- x_range[0]) + 1 + x_range[0] 
-	var random_y =  randi() % int(y_range[1]-y_range[0]) + 1 + y_range[0]
-	var random_pos = Vector2(random_x, random_y)
-	emerald.position = random_pos
+	emerald.position = gen_rand_pos()
 	emerald.connect("hit", self, "new_emerald")
-	emerald.connect("hit", self, "update_score")
+	emerald.connect("hit", self, "update_score", [500])
 	
 
 func gamer_over():
@@ -49,6 +46,7 @@ func gamer_over():
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
 	get_tree().call_group("coins", "queue_free")
+	get_tree().call_group("emeralds", "queue_free")
 	$Music.stop()
 	$DeathSound.play() 
 	
@@ -61,26 +59,20 @@ func new_game():
 	$Music.play()
 	new_coin()
 	new_emerald()
-	
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
 	$MobPath/MobSpawnLocation.offset = randi()
 	# Create a Mob instance and add it to the scene.
 	var mob = Mob.instance()
-#	add_child(mob)
 	call_deferred("add_child", mob)
 	# Set the mob's direction perpendicular to the path direction.
 	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
 	# Set the mob's position to a random location.
 	mob.position = $MobPath/MobSpawnLocation.position
-	# Add some randomness to the direction.
-#	direction += rand_range(-PI / 4, PI / 4)
-#	mob.rotation = direction
 	# Set the velocity (speed & direction).
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
-
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
